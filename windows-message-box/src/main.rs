190b1f560e544f37ebf425d0c5a9fde9701e6e25
poychang <poypost@gmail.com>
2022-10-06 17:18:12 +0800

    add windows-message-box sample

---
+++ windows-message-box/src/main.rs
@@ -0,0 +1,9 @@
+use windows::core::*;
+use windows::Win32::UI::WindowsAndMessaging::*;
+
+fn main() {
+    unsafe {
+        MessageBoxA(None, s!("Ansi"), s!("World"), MB_OK);
+        MessageBoxW(None, w!("Wide"), w!("World"), MB_OK);
+    }
+}
